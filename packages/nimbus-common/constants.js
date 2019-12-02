1ebe44f3d58c2d04b3dc8efcbf0886c006113d2e
Miles Johnson <miles.johnson@airbnb.com>
2019-12-02 13:07:04 -0800

    fix: Removed `css` and `scss` from asset file list.

--- packages/nimbus-common/constants.js
+++ packages/nimbus-common/constants.js
@@ -6,7 +6,7 @@ exports.EXTS_GROUP = '{ts,tsx,js,jsx}';
 
 exports.TJSX_EXT_PATTERN = /\.m?(t|j)sx?$/;
 
-exports.ASSET_EXT_PATTERN = /\.(ttf|eot|otf|svg|woff|woff2|mp3|png|jpg|jpeg|gif|ico|css|scss)$/;
+exports.ASSET_EXT_PATTERN = /\.(ttf|eot|otf|svg|woff|woff2|mp3|png|jpg|jpeg|gif|ico)$/;
 
 exports.GQL_EXT_PATTERN = /\.(gql|graphql)$/;
 
